0cc82783108554488b7c9f5a9183082a8ddd747d
jasonleih <707809370@qq.com>
2019-12-28 20:53:28 +0800

    first step

--- bootsect.s
+++ bootsect.s
@@ -4,10 +4,26 @@
 
 .equ BOOTSEG, 0x07c0
 
+.text
 ljmp $BOOTSEG, $_bootstart
 
 _bootstart:
-	jmp _bootstart
+	mov $0x03, %ah
+	int $0x10
+	mov $BOOTSEG,%ax
+	mov %ax,%es
+	mov $_str,%bp
+	mov $0x1301,%ax
+	mov $0x0007,%bx
+	mov $16,%cx
+	int $0x10
+loop:
+	jmp loop
+
+
+_str:
+	.ascii "hello world!"
+	.byte 13,10,13,10
 
 .= 510
 
